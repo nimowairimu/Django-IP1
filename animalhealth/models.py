460da71d5fa5bbffa2490f3323631a9b0015162b
nimowairimu <nimowairimu25@gmail.com>
2021-03-22 00:18:51 +0300

    Add an articles model.

--- animalhealth/models.py
+++ animalhealth/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class AHEnthusiast(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    
+    class Meta:
+        ordering = ['first_name']
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    AHEnthusiast = models.ForeignKey(Editor)
+
+
