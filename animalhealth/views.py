b53ab7b48659783667e6c899eb9cf83c74e11f91
nimowairimu <nimowairimu25@gmail.com>
2021-03-21 23:02:34 +0300

    Add Postgres dependency to connect and communicate with Django.

--- animalhealth/views.py
+++ animalhealth/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -9,26 +9,8 @@ def welcome(request):
 
 def article_of_day(request):
     date = dt.date.today()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> {day}{date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render (request,'all-articles/today-article.html',{"date": date})
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
     
 def past_days_article(request,past_date):
@@ -43,14 +25,9 @@ def past_days_article(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+        assert False
 
+    if date == dt.date.today():
+        return redirect(article_of_day)
 
+    return render(request, 'all-articles/old-articles.html', {"date": date})
