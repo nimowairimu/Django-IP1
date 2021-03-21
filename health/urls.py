55188656cd43af856eb9e3069c3ed85f89b2e1bf
nimowairimu <nimowairimu25@gmail.com>
2021-03-21 13:00:19 +0300

    Create a new URLpattern that will show the day's article.

---
+++ health/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.welcome,name = 'welcome'),
+    path ('today/'),views.article_of_day,name='articleToday'
+]
---
+++ health/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+import datetime as dt 
+
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
+
+
+def article_of_day(request):
+    date = dt.date.today()
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for { day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+        '''
+    return HttpResponse(html)    
+
+def convert_dates(dates):
+    # Function that gets the weekday number for the date 
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
