2245832b9d6f975b1ed137dfaa5639cde78c351c
nimowairimu <nimowairimu25@gmail.com>
2021-03-22 11:09:01 +0300

    Add modes to the project admin page.

--- animalhealth/admin.py
+++ animalhealth/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Enthusiast,Article,Location
 
-# Register your models here.
+admin.site.register(Enthusiast)
+admin.site.register(Article)
+admin.site.register(Location)
+
